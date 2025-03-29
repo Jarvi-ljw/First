7b8897903d721af64466dfaac25d72fba1758630
Jarvi-ljw <3215613871@qq.com>
2025-03-29 22:42:35 +0800

    feat(Day1): 添加 GuiDemo1.java 文件
    - 新增一个简单的 Swing 程序，创建了一个包含按钮的窗口
    - 设置了窗体大小、标题和关闭方式
    - 使用流式布局管理组件

---
+++ My Java/src/Day1/GuiDemo1.java
@@ -0,0 +1,27 @@
+package Day1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GuiDemo1 {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();//创建窗体
+
+        JPanel panel = new JPanel();//创建面板
+
+        jf.setSize(400, 300);//设置窗体大小
+
+        jf.setTitle("GuiDemo");
+
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//设置关闭方式
+
+        jf.setLayout(new FlowLayout());//设置流式布局
+
+        JButton jb = new JButton("按钮");
+
+        panel.add(jb);//将按钮添加到面板中
+        jf.add(panel);
+
+        jf.setVisible(true);//设置窗体可见
+    }
+}
